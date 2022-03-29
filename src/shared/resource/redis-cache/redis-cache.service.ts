235f1958a2e7815090d12febfb759d4c48a3ca6a
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2022-03-29 13:32:14 +0500

    Changes in format

--- src/shared/resource/redis-cache/redis-cache.service.ts
+++ src/shared/resource/redis-cache/redis-cache.service.ts
@@ -1,11 +1,9 @@
-import {Cache} from 'cache-manager';
-import {CACHE_MANAGER, Inject, Injectable} from "@nestjs/common";
+import { Cache } from "cache-manager";
+import { CACHE_MANAGER, Inject, Injectable } from "@nestjs/common";
 
 @Injectable()
 export class RedisCacheService {
-  constructor(
-      @Inject(CACHE_MANAGER) private readonly cache: Cache,
-  ) {}
+  constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
   public async get(key: string): Promise<string> {
     return await this.cache.get(key);
   }
@@ -14,7 +12,7 @@ export class RedisCacheService {
     return await this.cache.set(key, value);
   }
 
-  public async del(key: string): Promise<void> {
+  public async delete(key: string): Promise<void> {
     return await this.cache.del(key);
   }
 }
